ff3a084381ce07b2c51add88a7393306a71bbde9
MiraiSomnus <rafiqgreene7@gmail.com>
2026-04-29 18:38:30 -0400

    SignUp validatator message update

--- src/middleware/userValidators.js
+++ src/middleware/userValidators.js
@@ -19,7 +19,7 @@ body('email')
  .trim()
  .escape()
  .isLength({min:8, max:64})
- .withMessage('A minimum of 8 characters are required'),
+  .withMessage('Password must have a minimum of 8 characters'),
 
 body('role')
  .optional()
